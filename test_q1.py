7f83c6a93fecf61ec2ca1feba4841371a06eb3b6
David Ly <d.ly@mail.utoronto.ca>
2017-09-27 02:24:07 -0400

    read file

--- test_q1.py
+++ test_q1.py
@@ -10,7 +10,12 @@ from __future__ import unicode_literals
 import nltk
 
 def main():
-    grammar = nltk.data.load('file:/h/u12/g6/00/lydavid1/Desktop/csc485/CSC485-A1/q1.cfg')
+
+
+	with open('q1.cfg', 'r') as afile:
+		cfg_string = file.read()
+
+    grammar = nltk.grammar.CFG.fromstring(cfg_string)
     sent = 'people walks their dogs in parks'.split()
     sr = nltk.ShiftReduceParser(grammar)
     for tree in sr.parse(sent): #.nbest_parse(sent):
