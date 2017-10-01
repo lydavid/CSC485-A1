95bfee14f60d532646e4f203f2e8202b13672bcf
David Ly <d.ly@mail.utoronto.ca>
2017-09-30 20:31:43 -0400

    Added necessary files to begin Q3.1

---
+++ q3_test.py
@@ -0,0 +1,63 @@
+#! /usr/bin/env python3.5
+""" Question 1 Test"""
+
+# Not necessary; good for python 2.7 compatibility
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import nltk
+
+def parse(sentence, grammar, parser, should_parse=True):
+
+	print()
+
+	head_string = '\n(' + sentence + ')'
+    if should_parse:
+        head_string = head_string + ' (Should parse)'
+    else:
+        head_string = head_string + ' (Should not parse)'
+    print(head_string + '\n')
+
+    # If a sentence is not accepted by our grammar, print: No parses
+
+    for tree in parser.parse_all(sentence.split()):
+        print(tree)
+
+
+def main():
+
+	# Import grammar from Grammar file
+	with open('Grammar', 'r') as afile:
+        cfg_string = afile.read()
+
+	# Import lexicon from Lexicon file and append it to our string
+	with open('Lexicon', 'r') as afile:
+        cfg_string = cfg_string + '\n' + afile.read()
+
+    # Build our grammar for testing
+    grammar = nltk.grammar.CFG.fromstring(cfg_string)
+
+    # Select our parser
+    parser = nltk.parse.BottomUpChartParser(grammar)
+
+    ### Tests for 3.1 ###
+    parse('Nadia left immediately', grammar, parser)
+    parse('the cat with the long soft fur slowly ate', grammar, parser)
+    parse('she arrived', grammar, parser)
+    parse('Nadia with the long soft fur slowly ate', grammar, parser, False)
+    parse('the cat with the tall her arrived', grammar, parser, False)
+
+
+    ### Test every line in a file ###
+    print('\nA1-test.txt\n')
+    with open('A1-test.txt', 'r') as afile:
+        sentences = afile.read()
+
+    sentence_list = sentences.split('\n')
+    for sentence in sentence_list:
+    	parse(sentence, grammar, parser)
+
+if __name__ == '__main__':
+    main()
